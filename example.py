8ac5c354dbe134bdb34425ddea602d2f02c80ce8
frostedoyster <bigi.f@libero.it>
2023-02-06 15:04:14 +0100

    Benchmark

--- example.py
+++ example.py
@@ -12,7 +12,7 @@ c_prefactors.argtypes = [
     ctypes.POINTER(ctypes.c_double),
 ]
 
-c_spherical_harmonics = lib.cartesian_spherical_harmonics_naive
+c_spherical_harmonics = lib.cartesian_spherical_harmonics_cache
 c_spherical_harmonics.restype = None
 c_spherical_harmonics.argtypes = [
     ctypes.c_uint,
@@ -70,11 +70,32 @@ def test_against_scipy(xyz: np.ndarray, l: int, m: int):
     assert np.allclose(sh_scipy_l_m, sh_sphericart_l_m), f"assertion failed for l={l}, m={m}"
 
 
-n_samples = 10
-l_max = 5
+n_samples = 10000
+l_max = 10
 xyz = np.random.rand(n_samples, 3)
 for l in range(0, l_max+1):
     for m in range(-l, l+1):
         test_against_scipy(xyz, l, m)
 
 print("Assertions passed successfully!")
+
+
+n_tries = 100
+import time
+prefactors = get_prefactors(l_max)
+start = time.time()
+for _ in range(100):
+    sh_sphericart = spherical_harmonics(l_max, xyz, prefactors, gradients=False)
+finish = time.time()
+print(f"We took {1000*(finish-start)/n_tries} ms")
+
+import torch
+import e3nn
+xyz = torch.tensor(xyz)
+sh = e3nn.o3.spherical_harmonics(l_max, xyz, normalize=False)  # allow compilation (??)
+start = time.time()
+for _ in range(100):
+    sh = e3nn.o3.spherical_harmonics(l_max, xyz, normalize=False)
+finish = time.time()
+print(f"e3nn took {1000*(finish-start)/n_tries} ms")
+
