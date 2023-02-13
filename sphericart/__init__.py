d33d897ae097e696087cdb84630c77d64fe71aff
frostedoyster <bigi.f@libero.it>
2023-02-13 21:10:53 +0100

    Python build system

--- sphericart/__init__.py
+++ sphericart/__init__.py
@@ -6,7 +6,7 @@ import os
 
 # Load shared object library and define functions upon importing the sphericart module
 path_here = os.path.dirname(os.path.abspath(__file__))
-lib = ctypes.cdll.LoadLibrary(path_here + "/../libsphericart.so")
+lib = ctypes.cdll.LoadLibrary(path_here + "/lib/libsphericart.so")
 
 c_prefactors_fun = lib.compute_sph_prefactors
 c_prefactors_fun.restype = None
