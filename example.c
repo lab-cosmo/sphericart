49974e3c330092a58075a974b0ebf2f67c639f96
Michele Ceriotti <michele.ceriotti@gmail.com>
2023-02-14 08:27:32 +0100

    Better comparison output from example

--- example.c
+++ example.c
@@ -84,6 +84,19 @@ int main(int argc, char *argv[]) {
     time_total = time2_total = 0;
     for (int i_try = 0; i_try < n_tries; i_try++) {
         gettimeofday(&start, NULL);
+        cartesian_spherical_harmonics_hybrid(n_samples, l_max, prefactors, xyz, sph, dsph); 
+        gettimeofday(&end, NULL);
+        time = (end.tv_sec + end.tv_usec / 1e6 - start.tv_sec - start.tv_usec / 1e6)/n_samples;
+        time_total += time; time2_total +=  time*time;
+    } 
+    printf("Call with derivatives (hybrid) took %f ± %f µs/sample\n", 
+            1e6*time_total/n_tries, 1e6*sqrt(time2_total/n_tries - 
+                                       (time_total/n_tries)*(time_total/n_tries))
+            );
+
+    time_total = time2_total = 0;
+    for (int i_try = 0; i_try < n_tries; i_try++) {
+        gettimeofday(&start, NULL);
         cartesian_spherical_harmonics(n_samples, 1, prefactors, xyz, sph, dsph); 
         gettimeofday(&end, NULL);
         time = (end.tv_sec + end.tv_usec / 1e6 - start.tv_sec - start.tv_usec / 1e6)/n_samples;
