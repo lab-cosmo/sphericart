095fae37e243c33730197a04acb046ec44272145
frostedoyster <bigi.f@libero.it>
2023-02-15 15:55:51 +0100

    Attempted Python documentation fix

--- docs/src/conf.py
+++ docs/src/conf.py
@@ -1,8 +1,11 @@
 import os
+import sys
 import subprocess
 from datetime import datetime
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
+sys.path.insert(0, os.path.abspath("."))
+sys.path.insert(0, ROOT)
 
 
 # -- Project information -----------------------------------------------------
