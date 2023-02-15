f4348d4a42fd63669715b11f1f3ad9437383914f
Michele Ceriotti <michele.ceriotti@gmail.com>
2023-02-15 09:29:11 +0100

    Few tweaks to the example

--- examples/c/example.c
+++ examples/c/example.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[]) {
     unsigned int n_samples = 10000;
     unsigned int n_tries = 100;
     unsigned int l_max = 10;
-    int c;
+    int c, k=0;
 
     // parse command line options
     while ((c = getopt (argc, argv, "l:s:t:")) != -1) {
@@ -40,6 +40,7 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    printf("Running with l_max= %d, n_try= %d, n_samples= %d\n", l_max, n_tries, n_samples);
     double *prefactors = (double*) malloc(sizeof(double)*(l_max+1)*(l_max+2)/2);
     compute_sph_prefactors(l_max, prefactors);
 
@@ -81,10 +82,13 @@ int main(int argc, char *argv[]) {
                                        (time_total/n_tries)*(time_total/n_tries))
             );
 
+
+    double *sph1 = (double*) malloc(sizeof(double)*n_samples*(l_max+1)*(l_max+1));
+    double *dsph1 = (double*) malloc(sizeof(double)*n_samples*3*(l_max+1)*(l_max+1));
     time_total = time2_total = 0;
     for (int i_try = 0; i_try < n_tries; i_try++) {
         gettimeofday(&start, NULL);
-        cartesian_spherical_harmonics_hybrid(n_samples, l_max, prefactors, xyz, sph, dsph); 
+        cartesian_spherical_harmonics_hybrid(n_samples, l_max, prefactors, xyz, sph1, dsph1); 
         gettimeofday(&end, NULL);
         time = (end.tv_sec + end.tv_usec / 1e6 - start.tv_sec - start.tv_usec / 1e6)/n_samples;
         time_total += time; time2_total +=  time*time;
@@ -94,9 +98,23 @@ int main(int argc, char *argv[]) {
                                        (time_total/n_tries)*(time_total/n_tries))
             );
 
-    double *sph1 = (double*) malloc(sizeof(double)*n_samples*(l_max+1)*(l_max+1));
-    double *dsph1 = (double*) malloc(sizeof(double)*n_samples*3*(l_max+1)*(l_max+1));
-    
+    k=0;
+    for (int l=0; l<(l_max+1); l++)
+    {
+        for (int m=-l; m<=l; m++) {
+            printf("L= %d   m= %d  \n", l, m);
+            if (fabs(sph[k+l+m]/sph1[k+l+m]-1)>1e-6) printf("!!!! ");
+            printf("SPH: %e, %e\n", sph[k+l+m], sph1[k+l+m]);
+            if (fabs(dsph[k+l+m]/dsph1[k+l+m]-1)>1e-6) printf("!!!! ");            
+            printf("DxSPH: %e, %e\n", dsph[k+l+m], dsph1[k+l+m]);
+            if (fabs(dsph[(l_max+1)*(l_max+1)+k+l+m]/dsph1[(l_max+1)*(l_max+1)+k+l+m]-1)>1e-6) printf("!!!! ");            
+            printf("DySPH: %e, %e\n", dsph[(l_max+1)*(l_max+1)+k+l+m], dsph1[(l_max+1)*(l_max+1)+k+l+m]);
+            if (fabs(dsph[2*(l_max+1)*(l_max+1)+k+l+m]/dsph1[2*(l_max+1)*(l_max+1)+k+l+m]-1)>1e-6) printf("!!!! ");            
+            printf("DzSPH: %e, %e\n", dsph[(l_max+1)*(l_max+1)*2+k+l+m], dsph1[(l_max+1)*(l_max+1)*2+k+l+m]);
+        }
+        k+=2*l+1;            
+        //printf("chk %e, %e\n", dsph[i], dsph1[i]);
+    }    
     printf("================ Low-l timings ===========\n");
 
     time_total = time2_total = 0;
@@ -306,7 +324,8 @@ time_total = time2_total = 0;
                                        (time_total/n_tries)*(time_total/n_tries))
             );
 
-    int k=0; l_max=4;
+    k=0;
+    l_max=4;
     for (int l=0; l<(l_max+1); l++)
     {
         for (int m=-l; m<=l; m++) {
