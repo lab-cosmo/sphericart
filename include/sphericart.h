05fe682170783608d634144d093a4773a565d4ee
Luthaf <luthaf@luthaf.fr>
2023-02-23 16:18:10 +0100

    Enable basic C API

--- include/sphericart.h
+++ include/sphericart.h
@@ -8,8 +8,16 @@ extern "C" {
 #endif
 
 
+void SPHERICART_EXPORT sphericart_compute_sph_prefactors(int l_max, double *factors);
 
-
+void SPHERICART_EXPORT sphericart_cartesian_spherical_harmonics(
+    int n_samples,
+    int l_max,
+    const double* prefactors,
+    const double *xyz,
+    double *sph,
+    double *dsph
+);
 
 
 #ifdef __cplusplus
--- src/sphericart-capi.cpp
+++ src/sphericart-capi.cpp
@@ -1,2 +1,18 @@
 #include "sphericart.hpp"
 #include "sphericart.h"
+
+
+extern "C" void sphericart_compute_sph_prefactors(int l_max, double *factors) {
+    sphericart::compute_sph_prefactors(l_max, factors);
+}
+
+extern "C" void sphericart_cartesian_spherical_harmonics(
+    int n_samples,
+    int l_max,
+    const double* prefactors,
+    const double *xyz,
+    double *sph,
+    double *dsph
+) {
+    sphericart::cartesian_spherical_harmonics(n_samples, l_max, prefactors, xyz, sph, dsph);
+}
