c4dec239f30c52e08613e2e75eb6ca884999b03c
felixmusil <felix.musil@epfl.ch>
2023-05-19 17:32:06 +0200

    fix  typo in cpp

--- sphericart-jax/src/jax.cpp
+++ sphericart-jax/src/jax.cpp
@@ -18,7 +18,7 @@ void cpu_sph_with_gradients(void *out_tuple, const void **in) {
   const bool normalized = *reinterpret_cast<const bool *>(in[1]);
   const size_t n_samples = *reinterpret_cast<const size_t *>(in[2]);
   const T *xyz = reinterpret_cast<const T*>(in[3]);
-  size_t xyz_length{xyz_length*3};
+  size_t xyz_length{n_samples*3};
   size_t sph_len{(l_max+1)*(l_max+1)*n_samples};
   size_t dsph_len{sph_len*3};
   // The output is stored as a list of pointers since we have multiple outputs
