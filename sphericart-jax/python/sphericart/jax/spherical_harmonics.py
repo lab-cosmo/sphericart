2127a5ccc8eb295c1ba20c4eeff9cb494ceb87da
frostedoyster <bigi.f@libero.it>
2023-10-12 20:47:50 +0200

    Update documentation

--- sphericart-jax/python/sphericart/jax/spherical_harmonics.py
+++ sphericart-jax/python/sphericart/jax/spherical_harmonics.py
@@ -4,20 +4,23 @@ from .sph import sph
 def spherical_harmonics(xyz, l_max, normalized):  # TODO: CHANGE xyz to first argument everywhere
     """Computes the Spherical harmonics and their derivatives within
     the JAX framework. See :py:class:`sphericart.SphericalHarmonics` for more details.
+    This function supports ``jit``, ``vmap``, and up to two rounds of forward and/or
+    backward automatic differentiation (``grad``, ``jacfwd``, ``jacrev``, ``hessian``, ...).
+    It does not support ``pmap``.
 
     Parameters
     ----------
+    xyz : jax array [..., 3]
+        single vector or set of vectors in 3D. All dimensions are optional except for the last
     l_max : int
         maximum order of the spherical harmonics (included)
     normalized : bool
-        should we compute cartesian (``normalized=False``) or normalized spherical harmonics
-    xyz : jax array [n_sample, 3]
-        set of n_sample vectors in 3D
+        whether the function computes Cartesian solid harmonics (``normalized=False``) or normalized spherical harmonics
 
     Returns
     -------
-    jax array [n_sample, (l_max+1)**2]
-        Spherical harmonics expension of `xyz`
+    jax array [..., (l_max+1)**2]
+        Spherical harmonics expansion of `xyz`
     """
     if xyz.shape[-1] != 3: raise ValueError("the last axis of xyz must have size 3")
     xyz = xyz.ravel().reshape(xyz.shape)  # make contiguous (???)
