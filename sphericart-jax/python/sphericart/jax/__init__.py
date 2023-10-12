4966a23688af0b9cea668a95352a364915097a30
frostedoyster <bigi.f@libero.it>
2023-10-12 20:59:18 +0200

    More small modifications

--- sphericart-jax/python/sphericart/jax/__init__.py
+++ sphericart-jax/python/sphericart/jax/__init__.py
@@ -6,7 +6,3 @@ from .spherical_harmonics import spherical_harmonics
 # register the CPU operation to xla
 for _name, _value in cpu_ops.registrations().items():
     jax.lib.xla_client.register_custom_call_target(_name, _value)
-
-
-from .dsph import dsph
-from .ddsph import ddsph
