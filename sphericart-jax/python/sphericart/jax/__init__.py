d659685cdf01510d7a6215f62911b8c98dc68a37
frostedoyster <bigi.f@libero.it>
2023-10-13 07:22:39 +0200

    Debug CI jax version

--- sphericart-jax/python/sphericart/jax/__init__.py
+++ sphericart-jax/python/sphericart/jax/__init__.py
@@ -1,8 +1,12 @@
 import jax
 from .lib import cpu_ops
 from .spherical_harmonics import spherical_harmonics
+import jaxlib
 
+import inspect
+print(inspect.signature(jaxlib.hlo_helpers.custom_call))
+raise ValueError()
 
 # register the CPU operation to xla
 for _name, _value in cpu_ops.registrations().items():
-    jax.lib.xla_client.register_custom_call_target(_name, _value)
+    jax.lib.xla_client.register_custom_call_target(_name, _value, platform="cpu")
