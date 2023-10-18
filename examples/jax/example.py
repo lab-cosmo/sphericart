893ac39aaa442b245a8f78a412e31090ac258c86
frostedoyster <bigi.f@libero.it>
2023-10-18 12:34:40 +0200

    Finish example

--- examples/jax/example.py
+++ examples/jax/example.py
@@ -12,11 +12,35 @@ normalized = True  # in this example, we always compute normalized spherical har
 sph = sphericart.jax.spherical_harmonics(xyz, l_max, normalized)
 
 # jit the function with jax.jit()
+# the l_max argument (position 1 in the signature) must be static
 jitted_sph_function = jax.jit(sphericart.jax.spherical_harmonics, static_argnums=1)
 
 # compute the spherical harmonics with the jitted function and check their values
-# against the non-jitted version:
+# against the non-jitted version
 jitted_sph = jitted_sph_function(xyz, l_max, normalized)
 assert jax.numpy.allclose(sph, jitted_sph)
 
-# calculate a scalar function 
+# calculate a scalar function of the spherical harmonics and take its gradient
+# with respect to the input Cartesian coordinates, as well as its hessian
+def scalar_output(xyz, l_max, normalized):
+    return jax.numpy.sum(
+        sphericart.jax.spherical_harmonics(xyz, l_max, normalized)
+    )
+grad = jax.grad(scalar_output)(xyz, l_max, normalized)
+hessian = jax.hesssian(scalar_output)(xyz, l_max, normalized)
+
+# calculate a function of the spherical harmonics that returns an array
+# and take its jacobian with respect to the input Cartesian coordinates,
+# both in forward mode and in reverse mode
+def array_output(xyz, l_max, normalized):
+    return jax.numpy.sum(
+        sphericart.jax.spherical_harmonics(xyz, l_max, normalized),
+        axis=0
+    )
+jacfwd = jax.jacfwd(array_output)(xyz, l_max, normalized)
+jacrev = jax.jacrev(array_output)(xyz, l_max, normalized)
+assert jax.numpy.allclose(jacfwd, jacrev)  # check that the two are the same
+
+# use vmap and compare the result with the original result:
+vmapped_sph = jax.vmap(sphericart.jax.spherical_harmonics, in_axes=(0, None, None))(xyz, l_max, normalized)
+assert jax.numpy.allclose(sph, vmapped_sph)
