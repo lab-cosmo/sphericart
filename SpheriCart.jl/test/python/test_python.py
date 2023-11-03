4cf03fa52d40dfc359dca6a551a73042f4ae184d
frostedoyster <bigi.f@libero.it>
2023-11-03 21:04:10 +0100

    Add SphericalHarmonics consistency test

--- SpheriCart.jl/test/python/test_python.py
+++ SpheriCart.jl/test/python/test_python.py
@@ -7,7 +7,7 @@ L_MAX = 25
 N_SAMPLES = 100
 
 
-def test_consistency():
+def test_consistency_solid_harmonics():
 
     jl = Julia(compiled_modules=False)
     jl_code = """
@@ -30,4 +30,30 @@ def test_consistency():
     julia_results = compute_solid_harmonics_from_array(xyz, L_MAX)
 
     python_results = sphericart.SphericalHarmonics(L_MAX, normalized=False).compute(xyz)
+    assert np.allclose(julia_results, python_results)
+
+
+def test_consistency_spherical_harmonics():
+
+    jl = Julia(compiled_modules=False)
+    jl_code = """
+    using Pkg
+    Pkg.activate("../../")
+    Pkg.instantiate()
+    using SpheriCart
+    using StaticArrays
+
+    function compute_solid_harmonics_from_array(xyz::Array{Float64,2}, L_MAX)
+        xyz_svectors = [SVector{3}(x) for x in eachrow(xyz)]  # Convert to an array of StaticArrays.SVector{3}
+        basis = SpheriCart.SphericalHarmonics(L_MAX)
+        results = SpheriCart.compute(basis, xyz_svectors)
+        return results
+    end
+    """
+    xyz = np.random.rand(N_SAMPLES, 3)
+    jl.eval(jl_code)
+    compute_solid_harmonics_from_array = jl.eval("compute_solid_harmonics_from_array")
+    julia_results = compute_solid_harmonics_from_array(xyz, L_MAX)
+
+    python_results = sphericart.SphericalHarmonics(L_MAX, normalized=True).compute(xyz)
     assert np.allclose(julia_results, python_results)
