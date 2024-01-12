5d7f512c49e9dae6dfb67780bca2cfd2311037b9
Nicholas Browning <nickjbrowning@gmail.com>
2024-01-12 01:22:04 +0100

    WIP: sphericart_cuda_stub

--- sphericart/src/sphericart_cuda_stub.cpp
+++ sphericart/src/sphericart_cuda_stub.cpp
@@ -7,11 +7,11 @@ SphericalHarmonics<T>::SphericalHarmonics(size_t l_max, bool normalized) {}
 
 template <typename T> SphericalHarmonics<T>::~SphericalHarmonics() {}
 
-template <typename T> void SphericalHarmonics<T>::compute(const T *xyz, const size_t nsamples,
+template <typename T>
+void SphericalHarmonics<T>::compute(const T *xyz, const size_t nsamples,
                                     bool compute_with_gradients,
                                     bool compute_with_hessian,
                                     size_t GRID_DIM_X, size_t GRID_DIM_Y,
-                                    T *sph, T *dsph = nullptr,
-                                    T *ddsph = nullptr) {
+                                    T *sph, T *dsph, T *ddsph) {
     throw std::runtime_error("sphericart was not compiled with CUDA support");
 }
