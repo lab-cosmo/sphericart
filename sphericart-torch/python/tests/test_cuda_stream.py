e1ebd9b8c17b241de02664ac69c29e9ed9cc9634
Nicholas Browning <nickjbrowning@gmail.com>
2024-01-17 11:47:20 +0100

    added a pytest for cuda stream.

---
+++ sphericart-torch/python/tests/test_cuda_stream.py
@@ -0,0 +1,31 @@
+import pytest
+import torch
+
+import sphericart.torch
+
+
+torch.manual_seed(0)
+
+
+@pytest.fixture
+def xyz():
+    return 6 * torch.randn(100, 3, dtype=torch.float64, requires_grad=True)
+
+
+def test_cpu_vs_cuda(xyz):
+    if torch.cuda.is_available():
+        xyz_cuda = xyz.to("cuda")
+
+        calculator = sphericart.torch.SphericalHarmonics(l_max=20, normalized=False)
+        
+        s1 = torch.cuda.Stream()
+        s2 = torch.cuda.Stream()
+        
+        with torch.cuda.stream(s1):
+            sph_1, grad_sph_1 = calculator.compute_with_gradients(xyz)
+    
+        with torch.cuda.stream(s2):
+            sph_2, grad_sph_2 = calculator.compute_with_gradients(xyz)
+            
+        assert torch.allclose(sph_1, sph_2)
+        assert torch.allclose(grad_sph_1, grad_sph_2)
