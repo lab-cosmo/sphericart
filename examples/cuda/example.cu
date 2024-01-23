576fd6763f0ec01edfa7b2111120270c337f4007
Michele Ceriotti <michele.ceriotti@gmail.com>
2024-01-23 20:39:20 +0100

    Make CUDA example do something more meaningful

--- examples/cuda/example.cu
+++ examples/cuda/example.cu
@@ -46,6 +46,18 @@ int main() {
     auto ddsph =
         std::vector<double>(n_samples * 3 * 3 * (l_max + 1) * (l_max + 1), 0.0);
 
+    // the class is templated, so one can also use 32-bit float operations
+    auto xyz_f = std::vector<float>(n_samples * 3, 0.0);
+    for (size_t i = 0; i < n_samples * 3; ++i) {
+        xyz_f[i] = (float)xyz[i];
+    }
+    auto sph_f = std::vector<float>(n_samples * (l_max + 1) * (l_max + 1), 0.0);
+    auto dsph_f =
+        std::vector<float>(n_samples * 3 * (l_max + 1) * (l_max + 1), 0.0);
+    auto ddsph_f =
+        std::vector<float>(n_samples * 3 * 3 * (l_max + 1) * (l_max + 1), 0.0);
+
+
     /* ===== API calls ===== */
 
     // internal buffers and numerical factors are initalized at construction
@@ -67,9 +79,34 @@ int main() {
                    n_samples * (l_max + 1) * (l_max + 1) * sizeof(double),
                    cudaMemcpyDeviceToHost));
 
-    for (int i = 0; i < 4; i++) {
-        std::cout << sph[i] << std::endl;
+    // float version
+    sphericart::cuda::SphericalHarmonics<float> calculator_cuda_f(l_max);
+
+    float *xyz_cuda_f;
+    CUDA_CHECK(cudaMalloc(&xyz_cuda_f, n_samples * 3 * sizeof(float)));
+    CUDA_CHECK(cudaMemcpy(xyz_cuda_f, xyz_f.data(), n_samples * 3 * sizeof(float),
+                          cudaMemcpyHostToDevice));
+    double *sph_cuda_f;
+    CUDA_CHECK(cudaMalloc(&sph_cuda_f, n_samples * (l_max + 1) * (l_max + 1) *
+                                         sizeof(float)));
+
+    calculator_cuda_f.compute(xyz_cuda_f, n_samples, false, false,
+                            sph_cuda_f); // no gradients */
+
+    CUDA_CHECK(
+        cudaMemcpy(sph_f.data(), sph_cuda_f,
+                   n_samples * (l_max + 1) * (l_max + 1) * sizeof(float),
+                   cudaMemcpyDeviceToHost));
+
+    /* ===== check results ===== */
+
+    double sph_error = 0.0, sph_norm = 0.0;
+    for (size_t i = 0; i < n_samples * (l_max + 1) * (l_max + 1); ++i) {
+        sph_error += (sph_f[i] - sph[i]) * (sph_f[i] - sph[i]);
+        sph_norm += sph[i] * sph[i];
     }
+    printf("Float vs double relative error: %12.8e\n",
+           sqrt(sph_error / sph_norm));
 
     return 0;
 }
