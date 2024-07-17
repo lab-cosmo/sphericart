febf35c80cbe430c6bd20fa5a3481b51615c2be8
frostedoyster <bigi.f@libero.it>
2024-07-17 11:41:33 +0200

    Small changes to torch test

--- sphericart-torch/python/tests/test_no_points.py
+++ sphericart-torch/python/tests/test_no_points.py
@@ -14,34 +14,29 @@ def test_error(l_max, normalized):
 
     calculator = sphericart.torch.SphericalHarmonics(l_max=l_max, normalized=normalized)
 
-    try:
-        sph = calculator.compute(xyz)
-    except RuntimeError as e:
-        pytest.fail(f"compute threw an error: {e}")
+    sph = calculator.compute(xyz)
+    assert sph.shape == (0, (l_max + 1) ** 2)
 
-    try:
-        sph, grad_sph = calculator.compute_with_gradients(xyz)
-    except RuntimeError as e:
-        pytest.fail(f"compute_with_gradients threw an error: {e}")
+    sph, grad_sph = calculator.compute_with_gradients(xyz)
+    assert sph.shape == (0, (l_max + 1) ** 2)
+    assert grad_sph.shape == (0, 3, (l_max + 1) ** 2)
 
-    try:
-        sph, grad_sph, hess_sph = calculator.compute_with_hessians(xyz)
-    except RuntimeError as e:
-        pytest.fail(f"compute_with_hessians threw an error: {e}")
+    sph, grad_sph, hess_sph = calculator.compute_with_hessians(xyz)
+    assert sph.shape == (0, (l_max + 1) ** 2)
+    assert grad_sph.shape == (0, 3, (l_max + 1) ** 2)
+    assert hess_sph.shape == (0, 3, 3, (l_max + 1) ** 2)
 
     if torch.cuda.is_available():
         xyz_cuda = xyz.to("cuda")
-        try:
-            sph = calculator.compute(xyz_cuda)
-        except RuntimeError as e:
-            pytest.fail(f"compute threw an error: {e}")
 
-        try:
-            sph, grad_sph = calculator.compute_with_gradients(xyz_cuda)
-        except RuntimeError as e:
-            pytest.fail(f"compute_with_gradients threw an error: {e}")
+        sph = calculator.compute(xyz_cuda)
+        assert sph.shape == (0, (l_max + 1) ** 2)
 
-        try:
-            sph, grad_sph, hess_sph = calculator.compute_with_hessians(xyz_cuda)
-        except RuntimeError as e:
-            pytest.fail(f"compute_with_hessians threw an error: {e}")
+        sph, grad_sph = calculator.compute_with_gradients(xyz_cuda)
+        assert sph.shape == (0, (l_max + 1) ** 2)
+        assert grad_sph.shape == (0, 3, (l_max + 1) ** 2)
+
+        sph, grad_sph, hess_sph = calculator.compute_with_hessians(xyz_cuda)
+        assert sph.shape == (0, (l_max + 1) ** 2)
+        assert grad_sph.shape == (0, 3, (l_max + 1) ** 2)
+        assert hess_sph.shape == (0, 3, 3, (l_max + 1) ** 2)
