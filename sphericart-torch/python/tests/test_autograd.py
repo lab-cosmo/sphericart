9eea5cb54b274bc6dc362e68f3ecd3164d8f1bb4
frostedoyster <bigi.f@libero.it>
2024-08-22 16:24:51 +0200

    Test with desiderata

--- sphericart-torch/python/tests/test_autograd.py
+++ sphericart-torch/python/tests/test_autograd.py
@@ -89,3 +89,48 @@ def test_autograd_hessian(xyz):
 
     proportionality_factor = analytical_hessian[2, 2] / hessian[2, 2]
     assert torch.allclose(analytical_hessian, hessian * proportionality_factor)
+
+
+def test_third_derivative_error(xyz):
+    # Initialize a calculator
+    calculator = sphericart.torch.SphericalHarmonics(
+        l_max=8, normalized=False, backward_second_derivatives=True
+    )
+    # PR comment:
+    # Try False and you'll see that the failure point changes!
+    # Most likely conclusion: there is no silent failure, but removing the
+    # backward 2nd derivatives is still very desirable.
+
+    # Fill a single xyz point with arbitrary numbers
+    xyz = torch.tensor(
+        [
+            [0.67, 0.53, -0.22],
+        ],
+        requires_grad=True,
+    )
+
+    # Compute the spherical harmonics and run backward 3 times.
+    # The third one must raise.
+    sph = calculator.compute(xyz)
+    l0 = torch.sum(sph)
+    d1 = torch.autograd.grad(
+        outputs=l0,
+        inputs=xyz,
+        retain_graph=True,
+        create_graph=True,
+    )[0]
+    l1 = torch.sum(d1)
+    d2 = torch.autograd.grad(
+        outputs=l1,
+        inputs=xyz,
+        retain_graph=True,
+        create_graph=True,
+    )[0]
+    s2 = torch.sum(d2)
+    with pytest.raises(RuntimeError):
+        torch.autograd.grad(
+            outputs=s2,
+            inputs=xyz,
+            retain_graph=False,
+            create_graph=False,
+        )
