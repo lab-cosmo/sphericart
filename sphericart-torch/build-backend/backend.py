6f32cc25d843a2f6d39e7cbf7255d75a354bb011
Nick J. Browning <nickjbrowning@gmail.com>
2024-11-16 23:44:24 +0100

    updated backend.py

--- sphericart-torch/build-backend/backend.py
+++ sphericart-torch/build-backend/backend.py
@@ -1,26 +1,29 @@
-# this is a custom Python build backend wrapping setuptool's to set a
-# specific torch version as a build dependency, based on an environment
-# variable
+# This is a custom Python build backend wrapping setuptool's to add a build-time
+# dependencies on torch/cmake when building the wheel and not the sdist
 import os
-
 from setuptools import build_meta
 
-TORCH_VERSION = os.environ.get("SPHERICART_TORCH_TORCH_VERSION")
 
-if TORCH_VERSION is not None:
-    TORCH_DEP = f"torch =={TORCH_VERSION}"
+ROOT = os.path.realpath(os.path.dirname(__file__))
+
+FORCED_TORCH_VERSION = os.environ.get("SPHERICART_TORCH_BUILD_WITH_TORCH_VERSION")
+if FORCED_TORCH_VERSION is not None:
+    TORCH_DEP = f"torch =={FORCED_TORCH_VERSION}"
 else:
-    TORCH_DEP = "torch >=1.13"
+    TORCH_DEP = "torch >=2.3"
 
+# ==================================================================================== #
+#                   Build backend functions definition                                 #
+# ==================================================================================== #
 
+# Use the default version of these
 prepare_metadata_for_build_wheel = build_meta.prepare_metadata_for_build_wheel
+get_requires_for_build_sdist = build_meta.get_requires_for_build_sdist
 build_wheel = build_meta.build_wheel
 build_sdist = build_meta.build_sdist
 
 
+# Special dependencies to build the wheels
 def get_requires_for_build_wheel(config_settings=None):
     defaults = build_meta.get_requires_for_build_wheel(config_settings)
-    return defaults + [TORCH_DEP]
-
-
-get_requires_for_build_sdist = build_meta.get_requires_for_build_sdist
+    return defaults + ["cmake", TORCH_DEP]
