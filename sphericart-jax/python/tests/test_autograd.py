c9e5653d8259a931977b37552448c60beeb21e8c
Rocco Meli <r.meli@bluemail.ch>
2025-04-17 08:16:37 +0200

    CI on Alps (#176)

--- sphericart-jax/python/tests/test_autograd.py
+++ sphericart-jax/python/tests/test_autograd.py
@@ -2,53 +2,46 @@ import jax
 import jax.numpy as jnp
 import jax.test_util as jtu
 import pytest
+from utils import jax_float64
 
 import sphericart.jax
 
 
-@pytest.mark.parametrize("normalized", [True, False])
+@pytest.mark.parametrize("normalized", [True, False], ids=["spherical", "solid"])
 def test_autograd(normalized):
-    # here, 32-bit numerical gradients are very noisy, so we use 64-bit
-    jax.config.update("jax_enable_x64", True)
+    # 32-bit numerical gradients are very noisy, so we use 64-bit
+    with jax_float64():
+        key = jax.random.PRNGKey(0)
+        xyz = 6 * jax.random.normal(key, (100, 3))
 
-    key = jax.random.PRNGKey(0)
-    xyz = 6 * jax.random.normal(key, (100, 3))
+        function = (
+            sphericart.jax.spherical_harmonics
+            if normalized
+            else sphericart.jax.solid_harmonics
+        )
 
-    function = (
-        sphericart.jax.spherical_harmonics
-        if normalized
-        else sphericart.jax.solid_harmonics
-    )
+        def compute(xyz):
+            sph = function(xyz, 4)
+            return jnp.sum(sph)
 
-    def compute(xyz):
-        sph = function(xyz, 4)
-        return jnp.sum(sph)
-
-    jtu.check_grads(compute, (xyz,), modes=["fwd", "bwd"], order=1)
-
-    # reset to 32-bit so this doesn't carry over to other tests
-    jax.config.update("jax_enable_x64", False)
+        jtu.check_grads(compute, (xyz,), modes=["fwd", "bwd"], order=1)
 
 
-@pytest.mark.parametrize("normalized", [True, False])
+@pytest.mark.parametrize("normalized", [True, False], ids=["spherical", "solid"])
 def test_autograd_second_derivatives(normalized):
-    # here, 32-bit numerical gradients are very noisy, so we use 64-bit
-    jax.config.update("jax_enable_x64", True)
+    # 32-bit numerical gradients are very noisy, so we use 64-bit
+    with jax_float64():
+        key = jax.random.PRNGKey(0)
+        xyz = 6 * jax.random.normal(key, (100, 3))
 
-    key = jax.random.PRNGKey(0)
-    xyz = 6 * jax.random.normal(key, (100, 3))
+        function = (
+            sphericart.jax.spherical_harmonics
+            if normalized
+            else sphericart.jax.solid_harmonics
+        )
 
-    function = (
-        sphericart.jax.spherical_harmonics
-        if normalized
-        else sphericart.jax.solid_harmonics
-    )
+        def compute(xyz):
+            sph = function(xyz, 4)
+            return jnp.sum(sph)
 
-    def compute(xyz):
-        sph = function(xyz, 4)
-        return jnp.sum(sph)
-
-    jtu.check_grads(compute, (xyz,), modes=["fwd", "bwd"], order=2)
-
-    # reset to 32-bit so this doesn't carry over to other tests
-    jax.config.update("jax_enable_x64", False)
+        jtu.check_grads(compute, (xyz,), modes=["fwd", "bwd"], order=2)
