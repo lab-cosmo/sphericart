47b0994d35b0a376d57c6085ecbbee01d045c490
Filippo Bigi <98903385+frostedoyster@users.noreply.github.com>
2026-06-17 18:56:45 +0200

    Restore `sphericart.torch._lib_path()` (#243)

--- sphericart-torch/python/sphericart/torch/__init__.py
+++ sphericart-torch/python/sphericart/torch/__init__.py
@@ -1,4 +1,5 @@
 from .e3nn import e3nn_spherical_harmonics, patch_e3nn, unpatch_e3nn  # noqa: F401
+from .register import _lib_path as _lib_path
 from .register import register
 from .spherical_hamonics import SolidHarmonics, SphericalHarmonics  # noqa: F401
 
